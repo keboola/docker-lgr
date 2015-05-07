17d133d43b5912da10c89aa0aa85cd5d73344612
Odin <odinuv@gmail.com>
2015-05-07 08:00:12 +0200

    fix: added custom packages

--- init.R
+++ init.R
@@ -4,4 +4,8 @@ install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'jsonlite', 'leap
 library('devtools')
 
 # install the R application
+install_github('keboola/redshift-r-client', ref = "master")
+install_github('keboola/provisioning-r-client', ref = "master")
+install_github('keboola/r-application', ref = "master")
+install_github('keboola/r-docker-application', ref = "master")
 install_github('keboola/lgr-docker-application', ref = "master")
