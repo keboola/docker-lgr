151d2aaee17eb572671c3b6d7acbcb2889d78bd3
Odin <odinuv@gmail.com>
2015-05-07 07:34:28 +0200

    refactor: put back packages

--- init.R
+++ init.R
@@ -1,8 +1,7 @@
 # install some commonly used packages
-#install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'jsonlite', 'leaps', 'plyr', 'rJava', 'RJDBC', 'devtools'), repos = 'http://cran.us.r-project.org')
+install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'jsonlite', 'leaps', 'plyr', 'rJava', 'RJDBC', 'devtools'), repos = 'http://cran.us.r-project.org')
 
-install.packages(c('devtools'), repos = 'http://cran.us.r-project.org'
 library('devtools')
 
 # install the R application
-#install_github('keboola/lgr-docker-application', ref = "master")
+install_github('keboola/lgr-docker-application', ref = "master")
