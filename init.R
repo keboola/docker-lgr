866332153b99dea076152e5d9e0f496244c411b6
Odin <odinuv@gmail.com>
2015-05-07 07:08:18 +0200

    refactor: removed packages for debugging

--- init.R
+++ init.R
@@ -1,6 +1,7 @@
 # install some commonly used packages
-install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'jsonlite', 'leaps', 'plyr', 'rJava', 'RJDBC', 'devtools'), repos = 'http://cran.us.r-project.org')
+#install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'jsonlite', 'leaps', 'plyr', 'rJava', 'RJDBC', 'devtools'), repos = 'http://cran.us.r-project.org')
 
+install.packages(c('devtools'), repos = 'http://cran.us.r-project.org'
 library('devtools')
 
 # install the R application
