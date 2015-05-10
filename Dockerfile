b2ab97409124f828ee2c0bb598ae7e851303edc0
Odin <odinuv@gmail.com>
2015-05-10 22:38:27 +0200

    fix: missing packages

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ MAINTAINER Ondrej Popelka <ondrej.popelka@keboola.com>
 
 # Install packages
 RUN yum -y install \
+	openssl \
+	openssl-devel \
 	libcurl \
 	libcurl-devel \
 	libxml2-devel \
-	libgit2-dev \
-	R \
-	git
+	git \
+	R
 
 # Create html folder under the R directory (name of directory depends on version) - required for some R packages
 RUN find /usr/share/doc/ -name R* -exec mkdir '{}/html' \;
--- init.R
+++ init.R
@@ -1,5 +1,8 @@
+# install really required packages
+install.packages(c('git2r', 'jsonlite', 'rJava', 'RJDBC'), repos = 'http://cran.us.r-project.org')
+
 # install some commonly used packages
-install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'jsonlite', 'leaps', 'plyr', 'rJava', 'RJDBC', 'devtools'), repos = 'http://cran.us.r-project.org')
+install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'leaps', 'plyr'), repos = 'http://cran.us.r-project.org')
 
 library('devtools')
 
@@ -8,4 +11,4 @@ install_github('keboola/redshift-r-client', ref = "master")
 install_github('keboola/provisioning-r-client', ref = "master")
 install_github('keboola/r-application', ref = "master")
 install_github('keboola/r-docker-application', ref = "master")
-install_github('keboola/lgr-docker-application', ref = "master")
+install_github('keboola/r-luckyguess', ref = "master")
